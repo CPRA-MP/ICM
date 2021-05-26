d0739bef38b87d8d2b28d2ad3f8e46fe6b669313
Eric <47019383+edwhite12@users.noreply.github.com>
2021-05-26 11:18:34 -0500

    update for executable paths

--- ICM.py
+++ ICM.py
@@ -655,16 +655,24 @@ bimode_dir = os.path.normpath('%s/%s' % (par_dir,inputs[4,1].lstrip().rstrip()))
 HSI_dir = os.path.normpath('%s/%s' % (par_dir,inputs[5,1].lstrip().rstrip()))
 ewe_dir = os.path.normpath('%s/%s' % (par_dir,inputs[6,1].lstrip().rstrip()))
 
+#default# hydro_exe_path = './hydro_v23.4.0' 
+#default# bidem_exe_path = './bidem_v23.0.0'
+#default# morph_exe_path = './morph_v23.1.0' 
+
+hydro_exe_path = inputs[7,1].lstrip().rstrip() # path to hydro executable - ** path is relative to the S##/G###/hydro        ** - use './' if running copy of executable that is saved in /hydro directory 
+bidem_exe_path = inputs[8,1].lstrip().rstrip() # path to bidem executable - ** path is relative to the S##/G###/bidem/REGION ** - use './' if running copy of executable that is saved in /bidem/REGION directory
+morph_exe_path = inputs[9,1].lstrip().rstrip() # path to morph executable - ** path is relative to the S##/G###              ** - use './' if running copy of executable that is saved in /G## directory
+
 # Configuration files used by various ICM components
-VegConfigFile = inputs[7,1].lstrip().rstrip()
-WMConfigFile = inputs[8,1].lstrip().rstrip()
-EHConfigFile = inputs[9,1].lstrip().rstrip()
-EHCellsFile = inputs[10,1].lstrip().rstrip()
-EHLinksFile = inputs[11,1].lstrip().rstrip()
-BIMHWFile = inputs[12,1].lstrip().rstrip()
-exist_cond_tag = inputs[13,1].lstrip().rstrip()
-fwoa_init_cond_tag = inputs[14,1].lstrip().rstrip()
-shallow_subsidence_column = int(inputs[15,1].lstrip().rstrip())
+VegConfigFile = inputs[10,1].lstrip().rstrip()
+WMConfigFile = inputs[11,1].lstrip().rstrip()
+EHConfigFile = inputs[12,1].lstrip().rstrip()
+EHCellsFile = inputs[13,1].lstrip().rstrip()
+EHLinksFile = inputs[14,1].lstrip().rstrip()
+BIMHWFile = inputs[15,1].lstrip().rstrip()
+exist_cond_tag = inputs[16,1].lstrip().rstrip()
+fwoa_init_cond_tag = inputs[17,1].lstrip().rstrip()
+shallow_subsidence_column = int(inputs[18,1].lstrip().rstrip())
 
 compartment_output_file = 'compartment_out.csv'
 grid_output_file        = 'grid_500m_out.csv'
@@ -672,26 +680,26 @@ EHInterfaceFile         = 'ICM_info_into_EH.txt'
 
 
 # Filenames for Veg model input
-WaveAmplitudeFile = inputs[16,1].lstrip().rstrip()
-MeanSalinityFile = inputs[17,1].lstrip().rstrip()
-SummerMeanWaterDepthFile = inputs[18,1].lstrip().rstrip()
-SummerMeanSalinityFile = inputs[19,1].lstrip().rstrip()
-SummerMeanTempFile = inputs[20,1].lstrip().rstrip()
-TreeEstCondFile = inputs[21,1].lstrip().rstrip()
-HtAbvWaterFile = inputs[22,1].lstrip().rstrip()
-PerLandFile = inputs[23,1].lstrip().rstrip()
-PerWaterFile = inputs[24,1].lstrip().rstrip()
-AcuteSalFile = inputs[25,1].lstrip().rstrip()
+WaveAmplitudeFile = inputs[19,1].lstrip().rstrip()
+MeanSalinityFile = inputs[20,1].lstrip().rstrip()
+SummerMeanWaterDepthFile = inputs[21,1].lstrip().rstrip()
+SummerMeanSalinityFile = inputs[22,1].lstrip().rstrip()
+SummerMeanTempFile = inputs[23,1].lstrip().rstrip()
+TreeEstCondFile = inputs[24,1].lstrip().rstrip()
+HtAbvWaterFile = inputs[25,1].lstrip().rstrip()
+PerLandFile = inputs[26,1].lstrip().rstrip()
+PerWaterFile = inputs[27,1].lstrip().rstrip()
+AcuteSalFile = inputs[28,1].lstrip().rstrip()
 
 ## Simulation Settings
-startyear = int(inputs[26,1].lstrip().rstrip())
-endyear = int(inputs[27,1].lstrip().rstrip())
-ncycle = int(inputs[28,1].lstrip().rstrip())
-startyear_cycle = int(inputs[29,1].lstrip().rstrip())
-endyear_cycle = int(inputs[30,1].lstrip().rstrip())
-inputStartYear = int(inputs[31,1].lstrip().rstrip())
-nvegtype = int(inputs[32,1].lstrip().rstrip())
-update_hydro_attr = int(inputs[33,1].lstrip().rstrip())
+startyear = int(inputs[29,1].lstrip().rstrip())
+endyear = int(inputs[30,1].lstrip().rstrip())
+ncycle = int(inputs[31,1].lstrip().rstrip())
+startyear_cycle = int(inputs[32,1].lstrip().rstrip())
+endyear_cycle = int(inputs[33,1].lstrip().rstrip())
+inputStartYear = int(inputs[34,1].lstrip().rstrip())
+nvegtype = int(inputs[35,1].lstrip().rstrip())
+update_hydro_attr = int(inputs[36,1].lstrip().rstrip())
 
 # convert calendar years to elapsed years
 hotstart_year = startyear_cycle
@@ -705,28 +713,29 @@ cycle_end_elapsed = endyear_cycle - startyear + 1
 
 
 ## grid information for Veg ASCII grid files
-n500grid= int(inputs[34,1].lstrip().rstrip())
+n500grid= int(inputs[37,1].lstrip().rstrip())
 # n500gridveg = int(inputs[25,1].lstrip().rstrip()) #total number of grid cells in Veg model - including NoData cells
-n500rows = int(inputs[35,1].lstrip().rstrip())
-n500cols = int(inputs[36,1].lstrip().rstrip())
-xll500 = int(inputs[37,1].lstrip().rstrip())
-yll500 = int(inputs[38,1].lstrip().rstrip())
+n500rows = int(inputs[38,1].lstrip().rstrip())
+n500cols = int(inputs[39,1].lstrip().rstrip())
+xll500 = int(inputs[40,1].lstrip().rstrip())
+yll500 = int(inputs[41,1].lstrip().rstrip())
 
 ## grid information for EwE ASCII grid files
-n1000grid = int(inputs[39,1].lstrip().rstrip())
-n1000rows = int(inputs[40,1].lstrip().rstrip())
-n1000cols = int(inputs[41,1].lstrip().rstrip())
-xll1000 = inputs[42,1].lstrip().rstrip()
-yll1000 = inputs[43,1].lstrip().rstrip()
+n1000grid = int(inputs[42,1].lstrip().rstrip())
+n1000rows = int(inputs[43,1].lstrip().rstrip())
+n1000cols = int(inputs[44,1].lstrip().rstrip())
+xll1000 = inputs[45,1].lstrip().rstrip()
+yll1000 = inputs[46,1].lstrip().rstrip()
 
 # file naming convention settings
-mpterm = inputs[44,1].lstrip().rstrip()
-sterm = inputs[45,1].lstrip().rstrip()
-gterm = inputs[46,1].lstrip().rstrip()
-cterm = inputs[47,1].lstrip().rstrip()
-uterm = inputs[48,1].lstrip().rstrip()
-vterm = inputs[49,1].lstrip().rstrip()
-rterm = inputs[50,1].lstrip().rstrip()
+mpterm = inputs[47,1].lstrip().rstrip()
+sterm = inputs[48,1].lstrip().rstrip()
+gterm = inputs[49,1].lstrip().rstrip()
+cterm = inputs[50,1].lstrip().rstrip()
+uterm = inputs[51,1].lstrip().rstrip()
+vterm = inputs[52,1].lstrip().rstrip()
+rterm = inputs[53,1].lstrip().rstrip()
+
 
 # build some file naming convention tags
 runprefix = '%s_%s_%s_%s_%s_%s_%s' % (mpterm,sterm,gterm,cterm,uterm,vterm,rterm)
@@ -736,17 +745,17 @@ file_o_01_end_prefix = r'%s_O_01_%02d' % (runprefix,endyear-startyear+1)
 
 
 # 1D Hydro model information
-n_1D = int(inputs[51,1].lstrip().rstrip())
-RmConfigFile = inputs[52,1].lstrip().rstrip()
+n_1D = int(inputs[54,1].lstrip().rstrip())
+RmConfigFile = inputs[55,1].lstrip().rstrip()
 
 ## Barrier Island Model settings
-BITIconfig = inputs[53,1].lstrip().rstrip()
-n_bimode = int(inputs[54,1].lstrip().rstrip())
+BITIconfig = inputs[56,1].lstrip().rstrip()
+n_bimode = int(inputs[57,1].lstrip().rstrip())
 bimode_folders=[]
-for row in range(55,55+n_bimode):
+for row in range(58,58+n_bimode):
     bimode_folders.append(inputs[row,1].lstrip().rstrip())
 bidem_fixed_grids=[]
-for row in range(55+n_bimode,55+2*n_bimode):
+for row in range(58+n_bimode,58+2*n_bimode):
     bidem_fixed_grids.append(inputs[row,1].lstrip().rstrip())
 
 # read in asci grid structure
@@ -1704,7 +1713,7 @@ for year in range(startyear+elapsed_hotstart,endyear_cycle+1):
     print(' See %s for Hydro runtime logs.' % hydro_logfile)
 
     # run compiled Fortran executable - will automatically return to Python window when done running
-    hydrorun = subprocess.call('./hydro_v23.4.0')
+    hydrorun = subprocess.call(hydro_exe_path)
 
     if hydrorun != 0:
         print('******ERROR******')
@@ -2135,7 +2144,7 @@ for year in range(startyear+elapsed_hotstart,endyear_cycle+1):
 
         # run compiled Fortran executable - will automatically return to Python window when done running
         print(' Running BIDEM executable for %s region.' % fol)
-        bimoderun = subprocess.call('./bidem_v23.0.0') # os.system('./bidem_v23.0.0')
+        bimoderun = subprocess.call(bidem_exe_path)
 
         if bimoderun != 0:
             error_msg = '\n BIDEM model run for region %s year %s was unsuccessful.' % (fol,year)
@@ -2300,8 +2309,7 @@ for year in range(startyear+elapsed_hotstart,endyear_cycle+1):
         ip_csv.write(" %s - file naming convention prefix\n" % file_o_01_end_prefix)
 
 
-
-    morph_run = subprocess.call('./morph_v23.1.0')
+    morph_run = subprocess.call(morph_exe_path)
 
 
 
