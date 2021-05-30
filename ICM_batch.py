0b67b45c0dbe6bd2dc13b7140d72cea3b7d1a65c
Eric <47019383+edwhite12@users.noreply.github.com>
2021-05-30 10:18:07 -0500

    Update ICM_batch.py

--- ICM_batch.py
+++ ICM_batch.py
@@ -9,7 +9,7 @@ def write_sbatch(sbatch_file,account,email,tag,tag8,ctrl_str,ICMpy,perfflag=1):
         out = sbat.write('#SBATCH -p RM-shared\n')                              # use RM-shared partition
         out = sbat.write('#SBATCH -t 48:00:00\n')                               # set max simulation limit to 48 hours
         out = sbat.write('#SBATCH -N 1\n')                                      # use only one node
-        out = sbat.write('#SBATCH --ntasks-per-node 32\n')                      # use only 32 cores of one node
+        out = sbat.write('#SBATCH --ntasks-per-node 16\n')                      # use only 32 cores of one node
         out = sbat.write('#SBATCH -J %s\n' % tag8)                              # assign Job Name
         out = sbat.write('#SBATCH -o _%s.log\n' % tag)                          # set name for log file
         out = sbat.write('#SBATCH --mail-user=%s\n' % email)                    # set email address for notifications
