a98591c07a67d1379c5d01a0385d4ee0e1e092cd
Eric <47019383+edwhite12@users.noreply.github.com>
2021-07-28 01:34:31 -0500

    Update morph_differencing.py

--- morph_differencing.py
+++ morph_differencing.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import sys
 
 s   = int(sys.argv[1])
 g   = int(sys.argv[2])
@@ -12,10 +13,13 @@ yr0 = int(sys.argv[4])
 #yr0 = 2
 
 
+print('Comparing year %02d to year %02d for for S%02d G%03d:' % (yr1,yr0,s,g) )
+
 #############################
 ##      Setup folders      ##
 #############################
-out_fol         = './S%02d/G%03d/geomorph/output' % (s,g)
+print('   - setting up folders')
+out_fol         = 'S%02d/G%03d/geomorph/output' % (s,g)
 xyz_fol         = '%s/xyz' % out_fol 
 tif_fol         = '%s/tif' % out_fol
 png_fol         = '%s/png' % out_fol
@@ -29,13 +33,14 @@ for fol in [xyz_fol,tif_fol,png_fol,ts_fol]:
 ############################################
 ##      Calculate land change raster      ##
 ############################################
+print('   - calculating land change (binary file)')
 #lw0 = './S%02d/G%03d/geomorph/input/MP2023_S00_G500_C000_U00_V00_SLA_I_00_00_W_lndtyp30.xyz.b'
 ras1_bin_pth    = '%s/MP2023_S%02d_G%03d_C000_U00_V00_SLA_N_%02d_%02d_W_lndtyp30.xyz.b' % (out_fol,s,g,yr1,yr1)
 ras0_bin_pth    = '%s/MP2023_S%02d_G%03d_C000_U00_V00_SLA_N_%02d_%02d_W_lndtyp30.xyz.b' % (out_fol,s,g,yr0,yr0)
 ras01_bin_pth   = '%s/MP2023_S%02d_G%03d_C000_U00_V00_SLA_N_%02d_%02d_W_lndchange.xyz.b' % (out_fol,s,g,yr0,yr1)
 difftype        = 'dlw'
-nras_str        = 170852857
-noData_str      = -9999
+nras_str        = '170852857'
+noData_str      = '-9999'
 
 cmdstr1 = ['./morph_diff_v23.0.0', ras1_bin_pth, ras0_bin_pth, ras01_bin_pth, difftype, nras_str, noData_str]
 subprocess.call(cmdstr1)
@@ -44,6 +49,7 @@ subprocess.call(cmdstr1)
 #############################################################
 ##      Convert land change raster from binary to ASCI     ##
 #############################################################
+print('   - converting binary file to ASCI raster')
 xyz_asc_pth     = '%s/MP2023_S%02d_G%03d_C000_U00_V00_SLA_N_%02d_%02d_W_lndchange.xyz' % (xyz_fol,s,g,yr0,yr1)
 x_bin_pth       = '%s/raster_x_coord.b' % out_fol
 y_bin_pth       = '%s/raster_y_coord.b' % out_fol
@@ -56,7 +62,8 @@ subprocess.call(cmdstr2)
 #############################################################
 ##      Convert land change raster from ASCI to TIF        ##
 #############################################################
+print('   - converting ASCI raster to TIF')
 tif_pth     = '%s/MP2023_S%02d_G%03d_C000_U00_V00_SLA_N_%02d_%02d_W_lndchange.tif' % (tif_fol,s,g,yr0,yr1)
 
 cmdstr3 = ['gdal_translate', xyz_asc_pth, tif_pth]
-subprocess.call(cmdstr2)
+subprocess.call(cmdstr3)
