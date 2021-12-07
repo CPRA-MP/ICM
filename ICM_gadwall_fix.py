5e10831c4bc3134d8669e3da98b3e2be24008805
Eric White <47019383+edwhite12@users.noreply.github.com>
2021-12-07 16:07:49 -0600

    Update ICM_gadwall_fix.py

--- ICM_gadwall_fix.py
+++ ICM_gadwall_fix.py
@@ -63,6 +63,74 @@ print('\n CONFIGURING ICM MODEL FILES.')
 print('--------------------------------------------------')
 
 
+#Years to implement new files for hydro model
+hyd_switch_years = []
+hyd_file_orig = []
+hyd_file_new = []
+hyd_file_bk = []
+
+# Link numbers to be activated or deactivated in Hydro model
+links_to_change = []
+# Years (calendar year) to activate or deactivate each respective Hydro model links listed above
+link_years = []
+
+# full path to directory with FWA project GIS data files
+FWA_prj_input_dir_MC = '/ocean/projects/bcs200002p/ewhite12/MP2023/ICM/FWA_project_data/MC_elev_rasters'
+FWA_prj_input_dir_RR = '/ocean/projects/bcs200002p/ewhite12/MP2023/ICM/FWA_project_data/Ridge_Levee_elev_rasters'
+FWA_prj_input_dir_BS = '/ocean/projects/bcs200002p/ewhite12/MP2023/ICM/FWA_project_data/BS_SP_edge_erosion_multiplier_rasters'
+
+# Marsh creation project element IDs that are implemented in this simulation
+mc_elementIDs = []
+# Years (calendar year) to implement each respective marsh creation element IDs listed above
+mc_years = []
+# Marsh creation project element IDs that have deep water areas filled in (leave array empty if all use default shallow water fill threshold)
+mc_eid_with_deep_fill = []
+mc_depth_threshold_def = 0.762
+mc_depth_threshold_deep = 9999.999
+
+
+# link ID numbers for 'composite' marsh flow links (type 11) that need to be updated due to marsh creation projects
+mc_links = []
+# Years (calendar year) to update each respective'composite' marsh flow links (type 11) for marsh creation projects - this value should be the year after the project is implemented in the morphology model
+mc_links_years = []
+
+# Shoreline protection project IDs that are implemented in this simulation
+sp_projectIDs = []
+# Years (calendar year) to implement each respective shoreline protection project listed above
+sp_years = []
+
+# Levee and ridge project IDs that are implemented in this simulation
+rr_projectIDs = []
+# Years (calendar year) to implement each respective levee and ridge restoration project listed above
+rr_years = []
+
+
+
+# check that project implementation variables are of the correct lengths
+InputErrorFlag = 0
+InputErrorMsg = ''
+if len(link_years) != len(links_to_change):
+    InputErrorFlag = 1
+    InputErrorMsg = '%sNumber of links to activate/deactivate and implementation years are not equal in length!\n' % InputErrorMsg
+if len(mc_links_years) != len(mc_links):
+    InputErrorFlag = 1
+    InputErrorMsg = '%sNumber of links to update for marsh creation projects and implementation years are not equal in length!\n' % InputErrorMsg
+if len(mc_years) != len(mc_elementIDs):
+    InputErrorFlag = 1
+    InputErrorMsg = '%sMarsh Creation Project Implementation variables are not of equal length!\n' % InputErrorMsg
+if len(sp_years) != len(sp_projectIDs):d
+    InputErrorFlag = 1
+    InputErrorMsg = '%sShoreline Protection Project Implementation variables are not of equal length!\n' % InputErrorMsg
+if len(rr_years) != len(rr_projectIDs):
+    InputErrorFlag = 1
+    InputErrorMsg = '%sLevee & Ridge Project Implementation variables are not of equal length!\n' % InputErrorMsg
+
+if InputErrorFlag == 1:
+    print(' ***********Error with Project Implementation variables! Fix and re-run!\n')
+    sys.exit(InputErrorMsg)
+
+
+
 par_dir = os.getcwd()
 
 inputs = np.genfromtxt('ICM_gadwall_fix_control.csv',dtype=str,comments='#',delimiter=',')
