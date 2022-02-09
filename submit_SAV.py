28a06f64228ff4f3d4a6d7eb495ae0977ce24ce7
Eric White <47019383+edwhite12@users.noreply.github.com>
2022-02-08 22:50:31 -0600

    Create submit_SAV.py

---
+++ submit_SAV.py
@@ -0,0 +1,46 @@
+
+def write_sbatch(sbatch_file,account,email,tag8,ctrl_str,perfflag=1):
+    if perfflag == 1:
+            perfstr = '/usr/bin/time -v '
+    else:
+        perfstr = ''
+
+    with open(sbatch_file, mode='w') as sbat:
+        out = sbat.write('#!/bin/bash\n')
+        out = sbat.write('#SBATCH -p RM-shared\n')                              # use RM-shared partition
+        out = sbat.write('#SBATCH -t 01:30:00\n')                               # set max simulation limit to 48 hours
+        out = sbat.write('#SBATCH -N 1\n')                                      # use only one node
+        out = sbat.write('#SBATCH --ntasks-per-node 20\n')                      # use only 20 cores of one node
+        out = sbat.write('#SBATCH -J %s\n' % tag8)                              # assign Job Name
+        out = sbat.write('#SBATCH -o %s.log\n' % tag8)                          # set name for log file
+        out = sbat.write('#SBATCH --mail-user=%s\n' % email)                    # set email address for notifications
+#        out = sbat.write('#SBATCH --mail-type=begin\n')                         # send email when simulation leaves queue
+        out = sbat.write('#SBATCH --mail-type=end\n')                           # send email when simulation finishes
+        out = sbat.write('#SBATCH -A %s\n' % account)                           # XSEDE allocation for SU accounting
+        out = sbat.write('\n')
+        #out = sbat.write('date > run.begin\n')                                  # uncomment if want start datetime to be save to 'run.begin' text file
+        out = sbat.write('%s%s' % (perfstr, ctrl_str))                          # set call python script being submitted
+        #out = sbat.write('date > run.end\n')                                   # uncomment if want start datetime to be save to 'run.begin' text file
+    return()
+
+  
+import subprocess
+import os
+import sys
+import shutil
+
+s = int(sys.argv[1])
+g = int(sys.argv[2])
+sav_yr = int(sys.argv[3])
+
+perf    = 0                                                                     # turn on (1) or off (0) the verbose 'time' function to report out CPU and memory stats for run
+account = 'bcs200002p'                                                          # XSEDE allocation account
+email   = 'eric.white@la.gov'                                                   # emails for notification on queue
+sbatch_file = 'SAV_%04d.submit' % sav_yr
+
+
+cmd = 'python ICM_SAV.py %s %s %s\n' % (s, g, sav_yr)
+tag8    = 'sa%01d%03d%02d' % (s,g,y-sy+1) 
+write_sbatch(sbatch_file,account,email,tag8,ctrl_str,perf)
+cmdstr = ['sbatch', '%s' % sbatch_file]
+cmdout = subprocess.check_output(cmdstr).decode()
