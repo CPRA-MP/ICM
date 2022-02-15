aa0f97d951b4d3c9dc7f6b00fea22dff1b632300
Eric White <47019383+edwhite12@users.noreply.github.com>
2022-02-15 15:37:45 -0600

    Update submit_postprocess.py

--- submit_postprocess.py
+++ submit_postprocess.py
@@ -74,7 +74,7 @@ if rename_hydro == True:
     for out in os.listdir(out_fol):
         if out.endswith('.out'):
             out1 = '%s/%s' % (out_fol,out)
-            out2 = '%s/MP2023_S%02d_G%03d_C%03d_U%02d_V%02d_%r_O_01_52_H_%s' % (out_fol,s,g_fwa,c,u,v,r,out)
+            out2 = '%s/MP2023_S%02d_G%03d_C%03d_U%02d_V%02d_%s_O_01_52_H_%s' % (out_fol,s,g_fwa,c,u,v,r,out)
             if out.startswith('MP2023'):
                 print('%s already exists - skipping rename' % out1)
             else:
