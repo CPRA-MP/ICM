4ac28e2024dcfd910b031a6fe5797990ed1fe8f6
Eric <47019383+edwhite12@users.noreply.github.com>
2024-02-07 13:22:00 -0600

    clean repo for new structure for MP29
    starting with fresh repo directory that will now have folders for each ICM subroutine - started with the original MP23 version of ICM.py

--- sav_convert.bat
+++
@@ -1,9 +0,0 @@
-for y in 01 02 03 04 05 06 07 08 09 10 11 12 13
-do
-    geomorph/output/SAV/xyz/gdal_translate MP2023_S07_G500_C000_U00_V00_SLA_O_$y_$y_W_SAV.csvsptss.xyz     geomorph/output/SAV/tif/MP2023_S07_G500_C000_U00_V00_SLA_O_$y_$y_W_SAV.csvsptss.tif      &
-    geomorph/output/SAV/xyz/gdal_translate MP2023_S07_G500_C000_U00_V00_SLA_O_$y_$y_W_SAV.csvspsal.xyz     geomorph/output/SAV/tif/MP2023_S07_G500_C000_U00_V00_SLA_O_$y_$y_W_SAV.csvspsal.tif      &
-    geomorph/output/SAV/xyz/gdal_translate MP2023_S07_G500_C000_U00_V00_SLA_O_$y_$y_W_SAV.csvprob_pres.xyz geomorph/output/SAV/tif/MP2023_S07_G500_C000_U00_V00_SLA_O_$y_$y_W_SAV.csvprob_pres.tif  &
-    geomorph/output/SAV/xyz/gdal_translate MP2023_S07_G500_C000_U00_V00_SLA_O_$y_$y_W_SAV.csvprob_abs.xyz  geomorph/output/SAV/tif/MP2023_S07_G500_C000_U00_V00_SLA_O_$y_$y_W_SAV.csvprob_abs.tif   &
-    geomorph/output/SAV/xyz/gdal_translate MP2023_S07_G500_C000_U00_V00_SLA_O_$y_$y_W_SAV.csvprob.xyz      geomorph/output/SAV/tif/MP2023_S07_G500_C000_U00_V00_SLA_O_$y_$y_W_SAV.csvprob.tif       &
-    geomorph/output/SAV/xyz/gdal_translate MP2023_S07_G500_C000_U00_V00_SLA_O_$y_$y_W_SAV.csvdfl.xyz       geomorph/output/SAV/tif/MP2023_S07_G500_C000_U00_V00_SLA_O_$y_$y_W_SAV.csvdfl.tif        &
-done
