d25cbb05e034aa4d05722e4c623ee4bd65671594
Eric White <eric.davidson.white@gmail.com>
2026-09-18 16:14:55 -0500

    script to archive MP17 data from USGS SFTP

--- archive_MP2017_data.py
+++ archive_MP2017_data.py
@@ -6,7 +6,7 @@ import zipfile
 par = 'G:/Recovery Drive 4 - Good Data/ICM_simulations'
 parz = 'G:/MP17_archive'
 os.chdir(par)
-logfile = '%s/MP17_archive.log'
+logfile = '%s/MP17_archive.log' % parz
 
 
 
@@ -18,7 +18,7 @@ for g in Gs:
             sub = '%s/%s/%s' % (g,s,g)
 
             dirs2zip = []
-            dirs2zip.append('land_area_timeseries')
+            dirs2zip.append('land_area_time_series_plots')
             dirs2zip.append('QAQC_output')
             dirs2zip.append('ewe/EwEOutput/AreaAveraged')
             dirs2zip.append('geomorph/WM_StaticData.gdb')
@@ -31,7 +31,7 @@ for g in Gs:
             files2zip = []
             for bi in ['ST73124','ST73126','ST73129','ST73131','ST73139','ST73141']:
                 for y in range(1,51):
-                    files2zip.append('bimode/%s/profile%04d' % (bi,y) )
+                    files2zip.append('bimode/%s/results/profile_%04d' % (bi,y) )
             files2zip.append('ICM.py')
             files2zip.append('ICM_control.csv')
             files2zip.append('geomorph/input_params.csv')
@@ -44,50 +44,55 @@ for g in Gs:
 
             # build empty directory trees to hold zipped archives
             for bi in ['ST73124','ST73126','ST73129','ST73131','ST73139','ST73141']:
-                os.makedirs('%s/%s/bimode/%s' % (parz,sub,bi),exist_ok=True )
+                os.makedirs('%s/%s/bimode/%s/results' % (parz,sub,bi),exist_ok=True )
             os.makedirs('%s/%s/ewe'% (parz,sub),exist_ok=True )
             os.makedirs('%s/%s/geomorph'% (parz,sub),exist_ok=True )
             os.makedirs('%s/%s/hydro'% (parz,sub),exist_ok=True )
             os.makedirs('%s/%s/veg'% (parz,sub),exist_ok=True )
-                
-            for f2z in files2zip:
-                print('archiving %s' % f2z)
-                try:
-                    zip_success = 0  
-                    orig = '%s/%s/%s' % (par,sub,f2z)
-                    arc = '%s/%s/%s.zip' % (parz,sub,f2z)
-                    with zipfile.ZipFile(arc,mode='w',compression=zipfile.ZIP_STORED) as farc:
-                        farc.write(orig)
-                    zip_success = 1  
-                    if zip_success = 1:
-                        try:
-                            del_success = 0
-                            os.remove(orig)
-                            del_success = 1
-                        except:
-                            del_success = 0
-                except:
-                    zip_success = 0
-
-                with open(logfile,mode='a') as log:
-                    log.write('%s,%s,%d,%d\n' % (orig,arc,zip_success,del_success)
-
+            
             for d2z in dirs2zip:
                 print('archiving %s' % d2z)
                 try:
                     zip_success = 0 
+                    del_success = 0
                     orig = '%s/%s/%s' % (par,sub,d2z)
                     arc = '%s/%s/%s' % (parz,sub,d2z)
                     shutil.make_archive(arc,'zip',orig)
                     zip_success = 1  
-                    if zip_success = 1:
+                    if zip_success == 1:
                         try:
-                            del_success = 0
                             shutil.rmtree(orig)
                             del_success = 1
                         except:
                             del_success = 0
                 except:
                     zip_success = 0
+                    del_success = 0
                 with open(logfile,mode='a') as log:
-                    log.write('%s,%s,%d,%d\n' % (orig,arc,zip_success,del_success)
+                    log.write('%s,%s,%d,%d\n' % (orig,arc,zip_success,del_success) )                
+            
+            for f2z in files2zip:
+                print('archiving %s' % f2z)
+                try:
+                    zip_success = 0
+                    del_success = 0                    
+                    orig = '%s/%s/%s' % (par,sub,f2z)
+                    arc = '%s/%s/%s.zip' % (parz,sub,f2z)
+                    arcfname = orig.split('/')[-1]      # store just file name to drop all directories from archive structure
+                    with zipfile.ZipFile(arc,mode='w',compression=zipfile.ZIP_DEFLATED) as farc:
+                        farc.write(orig,arcfname)
+                    zip_success = 1  
+                    if zip_success == 1:
+                        try:
+                            os.remove(orig)
+                            del_success = 1
+                        except:
+                            del_success = 0
+                except:
+                    zip_success = 0
+                    del_success = 0 
+            
+                with open(logfile,mode='a') as log:
+                    log.write('%s,%s,%d,%d\n' % (orig,arc,zip_success,del_success) )
+
+
